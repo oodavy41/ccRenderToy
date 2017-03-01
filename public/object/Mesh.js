af99c89b19a00deb028e09c8acc5f6e3a6230285
CoColate <keke.ly@qq.com>
2017-03-01 23:24:50 +0800

    split into 3 comps

---
+++ public/object/Mesh.js
@@ -0,0 +1,12 @@
+class Mesh{
+    constructor(){
+        this.uvFlag=true;
+        this.vertices=[];
+        this.uvs=[];
+        this.normals=[];
+        this.colors=[];
+        this.indexs=[];
+    }
+
+    
+}
